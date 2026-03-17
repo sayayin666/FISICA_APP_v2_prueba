a6833e6aba64a6dd6992a12157628408cd177780
sayayin666 <sayayinjohan4@gmail.com>
2026-03-16 21:30:03 -0500

    initial commit

---
+++ principal/users/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
