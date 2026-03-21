76c3f0934c9a04845d5809710860b5e6eb9befa7
sayayin666 <sayayinjohan4@gmail.com>
2026-03-20 22:05:00 -0500

    tercer_cambio pt2

--- principal/users/api/serializer.py
+++ principal/users/api/serializer.py
@@ -7,10 +7,21 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'username',
+            'email',
             'first_name',
             'last_name',
             'is_staff',
             'is_active',
-            'date_joined',
-            'groups',
+            'password',
         ]
+    
+    def validate_email(self, value):
+        """Validar que el email sea único"""
+        user = self.instance
+        # Si es actualización y el email no cambió, permitir
+        if user and user.email == value:
+            return value
+        # Si el email ya existe, lanzar error
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Este email ya está registrado.")
+        return value
--- principal/users/api/views.py
+++ principal/users/api/views.py
@@ -3,8 +3,20 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from users.api.serializer import UserSerializer
 from users.models import User
+from django.contrib.auth.hashers import make_password
 
 class UserApiViewSet(ModelViewSet):
     permission_classes = [IsAdminUser]
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    
+    def create(self, request, *args, **kwargs):
+        request.data['password'] = make_password(request.data['password'])
+        return super().create(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        # Verificamos si la contraseña viene en la petición PATCH
+        if 'password' in request.data:
+            request.data['password'] = make_password(request.data['password'])
+        
+        return super().partial_update(request, *args, **kwargs)
